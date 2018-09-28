408cd63b737308630d141f6f8035b8fc291d79a3
Julian Johannesen <julian.johannesen@gmail.com>
2018-09-28 15:51:44 -0400

    Initial Commit of problem sets from .atom/.learn-ide/home/julian-johannesen

---
+++ problem-sets/javascript-fix-the-scope-lab-js-apply-000/index.js
@@ -0,0 +1,23 @@
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  var animal = "cat"
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+  return n + two
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+var theFunk = (funkyFunction())()
---
+++ problem-sets/js-basics-strings-code-along-v-000/index.js
@@ -0,0 +1,4 @@
+let currentUser = "Bob";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
